5fdfa0aa467b767a4bfea5f6f6ce1f558f4d61f6
Viktor Velev <viktorvelev8@gmail.com>
2019-11-16 22:48:44 +0100

    Implemented basic data preprocessing

--- src/main/java/com/swissquote/lauzhack/evolution/sq/team/SMA.scala
+++ src/main/java/com/swissquote/lauzhack/evolution/sq/team/SMA.scala
@@ -2,16 +2,16 @@ package com.swissquote.lauzhack.evolution.sq.team
 
 import org.apache.commons.collections4.queue.CircularFifoQueue
 
-
-
 class SMA(count: Int) extends Indicator {
   var buffer = new CircularFifoQueue[BigDecimal](count)
-  var sum = BigDecimal(0);
+  var sum = BigDecimal(0)
+
   def price(c: BigDecimal): BigDecimal = {
     if(buffer.isFull){
       var prev = buffer.peek()
       sum = sum +  (c - prev) * (1/count)
     }
+
     buffer.add(c)
     sum
   }
--- src/main/java/com/swissquote/lauzhack/evolution/sq/team/ScalaBBook.scala
+++ src/main/java/com/swissquote/lauzhack/evolution/sq/team/ScalaBBook.scala
@@ -1,8 +1,13 @@
 package com.swissquote.lauzhack.evolution.sq.team;
 
-import java.math.BigDecimal
+import java.util
 
+import scala.BigDecimal
 import com.swissquote.lauzhack.evolution.api.{BBook, Bank, Currency, Price, Trade}
+import org.apache.commons.collections4.queue.CircularFifoQueue
+import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.factory.Nd4j
+import org.nd4j.nativeblas.Nd4jCuda.NDArray
 import org.ta4j.core.{Bar, BarSeries, BaseBarBuilder, BaseBarSeriesBuilder}
 import org.ta4j.core.aggregator.BaseBarSeriesAggregator
 import org.ta4j.core.num.Num
@@ -10,44 +15,92 @@ import org.ta4j.core.num.Num
 import scala.collection.mutable
 
 object TeA {
+  // Technical Analysis
   def getIndicators = List(new SMA(4))
-
-
 }
 
 class ScalaBBook extends BBook {
-  implicit def long2decimal(num: Int): BigDecimal = new BigDecimal(num)
+  implicit def long2decimal(num: Int): BigDecimal = BigDecimal(num)
   private var bank: Bank = _
-  private var charts = new mutable.HashMap[(Currency, Currency), List[Indicator]]()
+  private var trainingMode = false
 
-  charts((Currency.EUR, Currency.CHF)) = TeA.getIndicators
-  charts((Currency.JPY, Currency.CHF)) = TeA.getIndicators
-  charts((Currency.USD, Currency.CHF)) = TeA.getIndicators
-  charts((Currency.GBP, Currency.CHF)) = TeA.getIndicators
+  /**
+   * A price table with all possible pairs of price changes and indicators
+   */
+  private var priceTable = new mutable.HashMap[(Currency, Currency), List[Indicator]]()
 
+  /**
+   * Windows using circular first in first out queue which updates automatically
+   * windowSize is variable
+   */
+  private var windowSize = 5
+
+  private var window =  new CircularFifoQueue[List[BigDecimal]](windowSize)
+  private var priceWindow = new CircularFifoQueue[Price](windowSize)
+
+  /**
+   * Holds the preprocessed dataset
+   */
+  private var dataSet = new mutable.MutableList[INDArray]
+
+  priceTable((Currency.EUR, Currency.CHF)) = TeA.getIndicators
+  priceTable((Currency.JPY, Currency.CHF)) = TeA.getIndicators
+  priceTable((Currency.USD, Currency.CHF)) = TeA.getIndicators
+  priceTable((Currency.GBP, Currency.CHF)) = TeA.getIndicators
 
   override def onInit(): Unit = {
     var builder = new BaseBarSeriesBuilder
 
-    // Start by buying some cash. Don't search for more logic here: numbers are just random..
-    bank.buy(new Trade(Currency.EUR, Currency.CHF, new BigDecimal(100000)))
-    bank.buy(new Trade(Currency.JPY, Currency.CHF, new BigDecimal(1000000)))
-    bank.buy(new Trade(Currency.USD, Currency.CHF, new BigDecimal(100000)))
-    bank.buy(new Trade(Currency.GBP, Currency.CHF, new BigDecimal(100000)))
-
+    // Start by buying some cash. Don't search for more logic here: numbers are just random.
+    bank.buy(new Trade(Currency.EUR, Currency.CHF, new java.math.BigDecimal(100000)))
+    bank.buy(new Trade(Currency.JPY, Currency.CHF, new java.math.BigDecimal(1000000)))
+    bank.buy(new Trade(Currency.USD, Currency.CHF, new java.math.BigDecimal(100000)))
+    bank.buy(new Trade(Currency.GBP, Currency.CHF, new java.math.BigDecimal(100000)))
   }
-  
-  override def onTrade(trade: Trade): Unit = {
 
+  override def onTrade(trade: Trade): Unit = {
     if (Math.random < 0.05) {
-      val coverTrade = new Trade(trade.base, trade.term, trade.quantity multiply 2)
+      val coverTrade = new Trade(trade.base, trade.term, trade.quantity.multiply(new java.math.BigDecimal(2)))
       bank.buy(coverTrade)
     }
   }
 
   override def onPrice(price: Price): Unit = {
-    val series = charts((price.base, price.term))
-    println(series map { _.price(price.rate) })
+
+    if (!trainingMode) {
+      println("Not training mode")
+
+      if(window.isFull) {
+        println("Window is ready...")
+        val data = preprocessData(window, BigDecimal(price.rate) > priceWindow.peek().rate)
+        // Feed to the network
+      }
+
+      val indicators = priceTable((price.base, price.term))
+      window.add(indicators map { _.price(price.rate) })
+      priceWindow.add(price)
+
+      println("WINDOW PEEK:" + window.peek())
+    }
+  }
+
+  def preprocessData(window: CircularFifoQueue[List[BigDecimal]], rising: Boolean): INDArray = {
+
+    val arraysToStack = new util.ArrayList[INDArray]()
+
+    window.forEach((el) => {
+      // Normalization here
+      val doubleArr: Array[Double] = el.map { _.toDouble }.toArray
+      arraysToStack.add(Nd4j.create(doubleArr))
+    })
+
+    val data = Nd4j.hstack(arraysToStack)
+    println(data)
+    dataSet += data
+    data
+  }
+
+  def pairData(): Unit = {
 
   }
 
