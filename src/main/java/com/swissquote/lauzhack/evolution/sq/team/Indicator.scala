a196b59ba87c7ef838a813ba0406b3796fc97a04
alex4o <mrabonin@gmail.com>
2019-11-16 21:58:49 +0200

    Created the indicators classes

---
+++ src/main/java/com/swissquote/lauzhack/evolution/sq/team/Indicator.scala
@@ -0,0 +1,5 @@
+package com.swissquote.lauzhack.evolution.sq.team
+
+trait Indicator {
+  def price(c: BigDecimal): BigDecimal;
+}
--- src/main/java/com/swissquote/lauzhack/evolution/sq/team/ScalaBBook.scala
+++ src/main/java/com/swissquote/lauzhack/evolution/sq/team/ScalaBBook.scala
@@ -3,12 +3,33 @@ package com.swissquote.lauzhack.evolution.sq.team;
 import java.math.BigDecimal
 
 import com.swissquote.lauzhack.evolution.api.{BBook, Bank, Currency, Price, Trade}
+import org.ta4j.core.{Bar, BarSeries, BaseBarBuilder, BaseBarSeriesBuilder}
+import org.ta4j.core.aggregator.BaseBarSeriesAggregator
+import org.ta4j.core.num.Num
+
+import scala.collection.mutable
+
+object TeA {
+  def getIndicators = List(new SMA(4))
+
+
+}
 
 class ScalaBBook extends BBook {
   implicit def long2decimal(num: Int): BigDecimal = new BigDecimal(num)
   private var bank: Bank = _
+  private var charts = new mutable.HashMap[(Currency, Currency), List[Indicator]]()
+
+  charts((Currency.EUR, Currency.CHF)) = TeA.getIndicators
+  charts((Currency.JPY, Currency.CHF)) = TeA.getIndicators
+  charts((Currency.USD, Currency.CHF)) = TeA.getIndicators
+  charts((Currency.GBP, Currency.CHF)) = TeA.getIndicators
+
 
   override def onInit(): Unit = {
+    var builder = new BaseBarSeriesBuilder
+
+
     // Start by buying some cash. Don't search for more logic here: numbers are just random..
     bank.buy(new Trade(Currency.EUR, Currency.CHF, new BigDecimal(100000)))
     bank.buy(new Trade(Currency.JPY, Currency.CHF, new BigDecimal(1000000)))
@@ -17,7 +38,12 @@ class ScalaBBook extends BBook {
 
   }
 
+
+
   override def onTrade(trade: Trade): Unit = {
+
+
+
     if (Math.random < 0.05) {
       val coverTrade = new Trade(trade.base, trade.term, trade.quantity multiply 2)
       bank.buy(coverTrade)
@@ -25,6 +51,8 @@ class ScalaBBook extends BBook {
   }
 
   override def onPrice(price: Price): Unit = {
+    val series = charts((price.base, price.term))
+    println(series map { _.price(price.rate) })
 
   }
 
