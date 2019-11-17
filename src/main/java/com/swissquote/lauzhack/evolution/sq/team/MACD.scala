b178fa6ad0b6123c7dbf702628eb9f37fe43551c
alex4o <mrabonin@gmail.com>
2019-11-17 03:17:19 +0200

    fixes one indicator

--- src/main/java/com/swissquote/lauzhack/evolution/sq/team/MACD.scala
+++ src/main/java/com/swissquote/lauzhack/evolution/sq/team/MACD.scala
@@ -2,6 +2,7 @@ package com.swissquote.lauzhack.evolution.sq.team
 
 class MACD(a: Indicator, b: Indicator) extends Indicator {
   override def price(c: BigDecimal): Option[BigDecimal] = {
+    if( a.last - b.last == 0) return None
     Some(a.last - b.last)
   }
 
